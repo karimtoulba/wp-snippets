5a410236e6d92c8a99360e37b554703b0ef3e694
Karim Toulba <karim@wadipurple.com>
2024-07-02 15:58:42 +0300

    Update woo-thank-you-message.php

--- woo-thank-you-message.php
+++ woo-thank-you-message.php
@@ -1,3 +1,7 @@
+<?php
+// Comment / <?php / before pasting in functions.php or Code Snippets
+
+// This will add a custom message on the WooCommerce Thank you page (depending on the current WPML language)
 add_filter( 'woocommerce_thankyou_order_received_text', 'misha_thank_you_subtitle', 20, 2 );
 
 function misha_thank_you_subtitle( $thank_you_title, $order ){
